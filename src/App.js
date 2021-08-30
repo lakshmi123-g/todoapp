c6f60140a6b0168a4b7461c3128b358eba6cca25
lakshmi123-g <81859808+lakshmi123-g@users.noreply.github.com>
2021-08-30 16:38:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import "./App.css";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { id: 1, title: "First Todo", complete: false },
+        { id: 2, title: "Second Todo", complete: true },
+        { id: 3, title: "Third Todo", complete: true },
+      ],
+    };
+  }
+  addTodo = (title) => {
+    console.log(title);
+    let todos = [...this.state.todos];
+    let id = todos[todos.length - 1] ? todos[todos.length - 1]["id"] + 1 : 0;
+    let newTodo = {
+      id,
+      title,
+      complete: false,
+    };
+    todos.push(newTodo);
+    this.setState({ todos });
+  };
+
+  completeTodo = (id) => {
+    let todos = [...this.state.todos];
+    todos.filter((todo) => {
+      if (todo.id === id) {
+        todo.complete = true;
+      }
+    });
+    this.setState({ todos });
+  };
+  deleteTodo = (id) => {
+    let todos = [...this.state.todos];
+    todos.filter((todo, index) => {
+      if (todo.id === id) {
+        todos.splice(index, 1);
+      }
+    });
+    this.setState({ todos });
+  };
+  render() {
+    return (
+      <div>
+        <div className="developer">
+          <span>Developer</span>
+          <br />
+          <a href="https://github.com/lakshmi123-g" target="_blank">
+            <b>lakshmi Durga</b>
+          </a>
+        </div>
+        <h1 className="heading">Todo Management Application with React</h1>
+        <div className="todos">
+          {this.state.todos.map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              completeTodo={(id) => this.completeTodo(id)}
+              deleteTodo={(id) => this.deleteTodo(id)}
+            />
+          ))}
+          <div id="down" />
+        </div>
+        <TodoForm addTodo={(todo) => this.addTodo(todo)} />
+      </div>
+    );
+  }
+}
